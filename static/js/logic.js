e595c1eb708c414e27546e68a2afbfea59a90e75
Matthew Lister <matthew.list56@outlook.com>
2023-04-27 18:33:23 -0500

    Map showing Earthquake locations made

--- static/js/logic.js
+++ static/js/logic.js
@@ -14,10 +14,51 @@ const loadData = () => {
 loadData()
     .then((data) => {
     console.log("Data loaded:", data);
+    createFeatures(data.features);
     })
 
-function HOLDER() {
+function createFeatures(featureData) {
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
+
+    let earthquakes = L.geoJSON(featureData, {
+        onEachFeature: onEachFeature
+    });
+
+    createMap(earthquakes);
 };
 
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(HOLDER);
+function createMap(earthquakeMapData){
+    
+    // Create the base layers.
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
 
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+
+    let baseMaps = {
+        "Street Map": street,
+        "Topographic Map": topo
+    };
+
+    // Create an overlay object to hold our overlay.
+    let overlayMaps = {
+        Earthquakes: earthquakeMapData
+    };
+    
+    let myMap = L.map("map", {
+        center: [
+          37.09, -95.71
+        ],
+        zoom: 5,
+        layers: [street, earthquakeMapData]
+    });
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+};
