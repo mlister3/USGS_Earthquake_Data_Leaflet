97a2d195ca6f7181f99cc063a13accf5314a48e6
Matthew Lister <matthew.list56@outlook.com>
2023-04-27 19:58:13 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,18 +18,26 @@ loadData()
     })
 
 function createFeatures(featureData) {
-    function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    
+    let earthquakes = L.geoJSON(featureData, { onEachFeature: onEachFeature });
+
+    function onEachFeature(feature, earthquakes) {
+        let magData = feature.properties.mag;
+        let radius = magData / 10;
+        let color = "blue"
+        let circle = L.circle(
+        {
+            radius: radius,
+            color: color,
+            fillOpacity: 0.5
+        }).bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+        earthquakes.addLayer(circle);
     }
 
-    let earthquakes = L.geoJSON(featureData, {
-        onEachFeature: onEachFeature
-    });
-
-    createMap(earthquakes, featureData);
+    createMap(earthquakes);
 };
 
-function createMap(earthquakeMapData, featureData){
+function createMap(earthquakeMapData){
 
     let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
@@ -39,31 +47,16 @@ function createMap(earthquakeMapData, featureData){
         "Topographic Map": topo
     };
 
-    // Create an overlay object to hold our overlay.
     let overlayMaps = {
         Earthquakes: earthquakeMapData
     };
-
-    var circleMarkers = L.layerGroup().addTo(myMap);
-
-    for (var i = 0; i < featureData.length; i++) {
-        let indEarthquake = featureData[i].properties;
-        let radius = indEarthquake.mag / 10;
-        let color = "blue"
-        let circle = L.circleMarker([featureData.coordinates], {
-            radius: radius,
-            color: color,
-            fillOpacity: 0.5
-        });
-        circleMarkers.addLayer(circle);
-    }
 
     let myMap = L.map("map", {
         center: [
           37.09, -95.71
         ],
         zoom: 3,
-        layers: [topo, earthquakeMapData, circleMarkers]
+        layers: [topo, earthquakeMapData]
     });
 
     L.control.layers(baseMaps, overlayMaps, {
